8c4a3826760474d0fe8758216427e8bcf6544ff6
SosSokleng99 <sossokleng16@kit.edu.kh>
2024-10-06 14:52:13 +0700

    Create Database and Configure Admin Site for LMS System

--- lms/admin.py
+++ lms/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Genre, Book, BookInstance
+
+# admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'isbn', 'display_genre']
+
+
+
+admin.site.register(Book, BookAdmin)
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ['__str__' ,'status', 'imprint', 'due_back']
+
+    list_filter = ('status', 'due_back')
+
+admin.site.register(BookInstance, BookInstanceAdmin)
+
+
+admin.site.register(Genre)
--- lms_project_b14/settings.py
+++ lms_project_b14/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'lms_project_b14.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # use PostgreSQL engine setting
+        'NAME': 'lms_batch14', # Database Name
+        'USER': 'postgres', # UserName
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost', 
+        'PORT': '5432', # Database PORT
     }
 }
 
